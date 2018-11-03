7693031b236fc56b5e196f7eda81e6a23299d411
zlyjoker102 <zlyjoker102@gmail.com>
2018-11-03 15:29:22 +0100

    dodanie total-cost component oraz styli css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CarsModule } from './cars/cars.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CarsModule,
     AppRoutingModule
   ],
   providers: [],
